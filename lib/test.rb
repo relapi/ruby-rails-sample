fb97352510a0943a001a221b1602b2209d393e14
relapi <staclagico@throwam.com>
2018-06-07 15:39:08 -0500

    Create test.rb

---
+++ lib/test.rb
@@ -0,0 +1,21 @@
+class Test
+  def one(a, b, c, d, e, f, g, h, j)
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    puts "one"
+    1+ 1
+  end
+end
